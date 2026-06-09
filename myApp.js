98c262bb9a91568517933d09096488138eb5a796
kailuatalahmeh-max <kailuatalahmeh@gmail.com>
2026-06-09 08:23:51 +0300

    edd route on myApp file

--- myApp.js
+++ myApp.js
@@ -1,40 +1,9 @@
-let express = require('express');
+let express = require("express");
 let app = express();
-console.log("Hello World")
+console.log("Hello World");
 
+app.get("/", function (req, res) {
+  res.send("Hello Express");
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
