7ad95b3ae5d2f81d1627ce5466e74cf919970d1d
kailuatalahmeh-max <kailuatalahmeh@gmail.com>
2026-06-10 09:27:19 +0300

    add .env file

--- myApp.js
+++ myApp.js
@@ -1,5 +1,7 @@
 let express = require("express");
 let app = express();
+require("dotenv").config();
+
 console.log("Hello World");
 
 app.use("/public", express.static(__dirname + "/public"));
@@ -9,8 +11,14 @@ app.get("/", function (req, res) {
 });
 
 app.get("/json", (req, res) => {
+  let message = "Hello json";
+
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    message = message.toUpperCase();
+  }
+
   res.json({
-    message: "Hello json"
+    message: message,
   });
 });
 
