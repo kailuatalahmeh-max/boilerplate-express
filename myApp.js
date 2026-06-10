acba4b677c35c092c16b3eba0ffc89fe75ffc0ad
kailuatalahmeh-max <kailuatalahmeh@gmail.com>
2026-06-10 04:00:43 +0300

    add route json in myApp file

--- myApp.js
+++ myApp.js
@@ -8,5 +8,10 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
+app.get("/json", (req, res) => {
+  res.json({
+    message: "Hello json"
+  });
+});
 
 module.exports = app;
